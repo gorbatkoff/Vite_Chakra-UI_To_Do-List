eb40b242007be189611a300ddd2a5d2997ac6907
gorbatkoff <gorbatkovartyom@gmail.com>
2023-04-17 18:48:52 +0300

    Add useColorMode hooks

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,16 +1,36 @@
 import {memo} from "react";
 import styles from "./Header.module.scss";
-import {Heading} from "@chakra-ui/react";
+import {Button, FormControl, FormLabel, Heading, Switch, useColorMode} from "@chakra-ui/react";
 
 interface HeaderProps {
     className?: string
 }
 
+
 export const Header = memo(({className}: HeaderProps) => {
+
+    const {colorMode, toggleColorMode} = useColorMode();
+
+    console.log(colorMode)
 
     return (
         <header className={styles.Header}>
             <Heading size='xl'>тасколист</Heading>
+
+            <div>
+                <FormControl display='flex' alignItems='center'>
+                    <FormLabel htmlFor='theme-switcher' mb='0'>
+                        <Heading size='md'>
+                            {colorMode === "dark" ? "Включить свет" : "Выключить свет"}
+                        </Heading>
+                    </FormLabel>
+                    <Switch
+                        colorScheme='purple'
+                        onChange={toggleColorMode}
+                        id='theme-switcher'
+                        size='md'/>
+                </FormControl>
+            </div>
         </header>
     );
 });
