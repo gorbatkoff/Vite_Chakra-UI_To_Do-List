8c717bfa2bb4853b79e662e867fdddc0f5b8f455
gorbatkoff <gorbatkovartyom@gmail.com>
2023-04-17 18:49:25 +0300

    Delete extendTheme and add custom ChakraUIProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {ChakraProvider, extendTheme} from '@chakra-ui/react'
-import App from './app/App'
 import './index.css'
+import React from 'react'
+import App from './app/App'
+import ReactDOM from 'react-dom/client'
 import {ProviderOfStore} from "/src/app/providers";
-
-const light = {
-    brand: {
-        900: '#141414',
-        800: '#153e75',
-        700: '#2a69ac',
-    },
-}
-
-const night = {
-    brand: {
-        900: '#262626',
-        800: '#123e75',
-        700: '#5aaca4',
-        100: '#ffffff',
-    },
-}
-
-const theme = extendTheme({light, night})
+import {ChakraUIProvider} from "/src/app/providers/ThemeProvider";
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ProviderOfStore>
-        <ChakraProvider theme={theme}>
+        <ChakraUIProvider>
             <App/>
-        </ChakraProvider>
+        </ChakraUIProvider>
     </ProviderOfStore>
 )
