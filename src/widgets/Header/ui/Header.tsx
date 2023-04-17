af4cb70e7337764f791106ae39ee06369a9b24a7
gorbatkoff <gorbatkovartyom@gmail.com>
2023-04-18 00:34:22 +0300

    Add ThemeSwitcher component

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,6 +1,7 @@
 import {memo} from "react";
 import styles from "./Header.module.scss";
-import {Button, FormControl, FormLabel, Heading, Switch, useColorMode} from "@chakra-ui/react";
+import {Heading} from "@chakra-ui/react";
+import {ThemeSwitcher} from "/src/shared/ui/ThemeSwitcher/ThemeSwitcher";
 
 interface HeaderProps {
     className?: string
@@ -9,28 +10,10 @@ interface HeaderProps {
 
 export const Header = memo(({className}: HeaderProps) => {
 
-    const {colorMode, toggleColorMode} = useColorMode();
-
-    console.log(colorMode)
-
     return (
         <header className={styles.Header}>
             <Heading size='xl'>тасколист</Heading>
-
-            <div>
-                <FormControl display='flex' alignItems='center'>
-                    <FormLabel htmlFor='theme-switcher' mb='0'>
-                        <Heading size='md'>
-                            {colorMode === "dark" ? "Включить свет" : "Выключить свет"}
-                        </Heading>
-                    </FormLabel>
-                    <Switch
-                        colorScheme='purple'
-                        onChange={toggleColorMode}
-                        id='theme-switcher'
-                        size='md'/>
-                </FormControl>
-            </div>
+            <ThemeSwitcher/>
         </header>
     );
 });
