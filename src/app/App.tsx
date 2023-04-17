49a32812a07c9ac13291ce1191c111ebac0f70d7
gorbatkoff <gorbatkovartyom@gmail.com>
2023-04-17 18:47:46 +0300

    Delete getting theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,12 @@
 import './App.css'
-import {Box, Heading, useTheme} from "@chakra-ui/react";
-import {useState} from "react";
+import {Box} from "@chakra-ui/react";
 import {Header} from "/src/widgets/Header";
 import {Content} from "/src/widgets/Content/Content";
 
-type themes = "light" | "night";
-
 function App() {
 
-    const [currentTheme, setCurrentTheme] = useState<themes>("night");
-    const theme = useTheme()[currentTheme];
-
     return (
-        <Box className={"App"} bg={theme.brand['900']} w='100%' p={5} color={theme.brand['100']}>
+        <Box className={"App"} p={5}>
             <Header/>
             <Content/>
         </Box>
