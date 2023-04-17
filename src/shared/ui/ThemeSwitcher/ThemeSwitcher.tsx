3c8998f69c2893f4f597e08c299d6e2812fa6679
gorbatkoff <gorbatkovartyom@gmail.com>
2023-04-18 00:38:23 +0300

    Create ThemeSwitcher scss file

---
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,27 @@
+import {memo} from "react";
+import {FormControl, FormLabel, Heading, Switch, useColorMode} from "@chakra-ui/react";
+
+interface ThemeSwitcherProps {
+    className?: string
+}
+
+export const ThemeSwitcher = memo((props: ThemeSwitcherProps) => {
+    const {colorMode, toggleColorMode} = useColorMode();
+
+    return (
+        <div>
+            <FormControl display='flex' alignItems='center'>
+                <FormLabel htmlFor='theme-switcher' mb='0'>
+                    <Heading size='md'>
+                        {colorMode === "dark" ? "Включить свет" : "Выключить свет"}
+                    </Heading>
+                </FormLabel>
+                <Switch
+                    colorScheme='purple'
+                    onChange={toggleColorMode}
+                    id='theme-switcher'
+                    size='md'/>
+            </FormControl>
+        </div>
+    );
+});
